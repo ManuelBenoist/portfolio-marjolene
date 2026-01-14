2d43fec7a73b8b619cbb8139ccf3269abf0d736d
Manuel BENOIST <manuelbenoist@gmail.com>
2026-01-14 12:53:18 +0100

    config: install Nuxt4 and Tailwind CSS

---
+++ app/app.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <NuxtWelcome />
+  </div>
+  <div>
+    <h1 class="text-3xl font-bold underline bg-amber-800">
+      Tailwind is working!
+    </h1>
+  </div>
+</template>
