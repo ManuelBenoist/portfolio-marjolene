955fb4abbf677c1f81e13fa55cd5eae0516f6e3c
Manuel BENOIST <manuelbenoist@gmail.com>
2026-01-15 11:57:37 +0100

    Fix: layouts Nuxt, suppression app.vue, layout par défaut fonctionnel

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -8,4 +8,5 @@
 </template>
 
 <script setup>
+  definePageMeta({ layout: false })
 </script>
---
+++ app/pages/test-layout.vue
@@ -0,0 +1,8 @@
+<template>
+	<div>Test du layout</div>
+</template>
+<script setup>
+definePageMeta({ layout: 'default' })
+// Aucun contenu, juste le layout
+// Aucun contenu, juste le layout
+</script>
